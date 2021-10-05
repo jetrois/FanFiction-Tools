106c832f54f50bac4faf09b52fd235ab6f51c04d
Jetrois <jetrois@pm.me>
2021-10-05 07:37:23 -0500

    Ran safari web extension converter Xcode 13 to enable support for safari extensions on mac Big Sur and iOS 15

---
+++ fanfiction-tools/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Jetoine on 10/5/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
